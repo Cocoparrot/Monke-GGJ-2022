2d1128ab31a412d12e7c074bb94c253d930320f1
JOSHUA THOMPSON <jt230724@falmouth.ac.uk>
2022-01-30 16:26:47 +0000

    Added cinmachine and comic

---
+++ Assets/ComicSwitcher.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using FMODUnity;
+
+public class ComicSwitcher : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public int panelCount;
+
+    public string[] cam;
+
+    public EventReference[] comicLines;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelCount = 1;
+        animator = GetComponent<Animator>();   
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            PanelSwitch();
+        }
+    }
+    // Update is called once per frame
+    void PanelSwitch()
+    {
+        if (panelCount <= 8)
+        {
+            animator.Play(cam[panelCount]);
+            RuntimeManager.PlayOneShot(comicLines[panelCount]);
+        }
+        panelCount += 1;
+        if (panelCount >= 10)
+        {
+            SceneManager.LoadScene("Level 1");
+        }
+    }
+}
